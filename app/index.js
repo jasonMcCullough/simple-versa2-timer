59f6d8dbdf53ccabead6a97a9d5c2d94206b444c
Jason McCullough <jasonrox@live.co.uk>
2022-05-15 18:21:48 +0100

    Better button layout + other misc improvements

--- app/index.js
+++ app/index.js
@@ -3,12 +3,19 @@ import { display } from "display";
 import { vibration } from "haptics";
 import * as document from "document";
 import { preferences } from "user-settings";
+import { me as device } from "device";
 import * as util from "../common/utils";
 
 const myLabel = document.getElementById("myLabel");
-const button = document.getElementById("button");
 const myTimer = document.getElementById("myTimer");
-const startTime = 60
+
+const button = document.getElementById("button");
+
+const pauseBtn = document.getElementById("pause");
+const resetBtn = document.getElementById("reset");
+
+// TODO: Make this configurable!
+const startTime = 60;
 
 var currentTime = startTime;
 var timerStarted = false;
@@ -16,20 +23,24 @@ var timerStarted = false;
 var interval = 1000; // ms
 var expected = Date.now() + interval;
 
+pauseBtn.style.display = "none";
+resetBtn.style.display = "none";
+
+console.log(`Model name:       ${device.modelName}`);
+console.log(`Firmware version: ${device.firmwareVersion}`);
+console.log(`Screen dimensions: ${device.screen.width} x ${device.screen.height}`);
+
 setTimeout(step, interval);
 function step() {
-  var dt = Date.now() - expected; // the drift (positive for overshooting)
-  // do what is to be done
+  // The drift
+  var dt = Date.now() - expected; 
+  
   button.onactivate = function(evt) {
-    if (button.text == "Pause Timer") {
-      timerStarted = false;
-      button.text = "Start Timer";
-      button.style.fill = "green";
-      
-    } else if (button.text == "Start Timer") {
+    if (button.text == "Start Timer") {
       timerStarted = true;
-      button.text = "Pause Timer";
-      button.style.fill = "orange";
+      button.style.display = "none";
+      pauseBtn.style.display = "inline";
+      resetBtn.style.display = "inline";
       
     } else if (button.text == "Reset Timer") {
       vibration.stop("alert");
@@ -42,6 +53,32 @@ function step() {
     }
   }
   
+  pauseBtn.onactivate = function(evt) {
+    if (pauseBtn.text != "Cont.") {
+      timerStarted = false;
+      pauseBtn.text = "Cont.";
+      pauseBtn.style.fill = "green";
+    } else if (pauseBtn.text == "Cont.") {
+      timerStarted = true;
+      pauseBtn.text = "Pause";
+      pauseBtn.style.fill = "orange";
+    }
+  }
+  
+  resetBtn.onactivate = function(evt) {
+    timerStarted = false;
+    currentTime = startTime;
+    myTimer.text = currentTime;
+    myLabel.text = "";
+    
+    pauseBtn.text = "Pause";
+    pauseBtn.style.fill = "orange";
+    
+    pauseBtn.style.display = "none";
+    resetBtn.style.display = "none";
+    button.style.display = "inline";
+  }
+  
   if(timerStarted && currentTime > 0) {
     currentTime = currentTime - 1;
     myTimer.text = currentTime;
@@ -49,7 +86,11 @@ function step() {
   } else if(!timerStarted && currentTime > 0) {
     myTimer.text = currentTime;
     
-  } else if(currentTime <= 0) {
+  } else if(currentTime <= 1) {
+    pauseBtn.style.display = "none";
+    resetBtn.style.display = "none";
+    button.style.display = "inline";
+    
     myTimer.text = "";
     myLabel.text = "Times up!";
     vibration.start("alert");
@@ -58,7 +99,7 @@ function step() {
     button.style.fill = "red"
   }
   
-  // https://stackoverflow.com/questions/29971898/how-to-create-an-accurate-timer-in-javascript
   expected += interval;
-  setTimeout(step, Math.max(0, interval - dt)); // take into account drift
+  // Take into account drift, reference: https://stackoverflow.com/questions/29971898/how-to-create-an-accurate-timer-in-javascript
+  setTimeout(step, Math.max(0, interval - dt)); 
 }
