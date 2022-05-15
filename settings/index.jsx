d45c813fa70f4c9380e896ddd17d87d2046563ef
Jason McCullough <jasonrox@live.co.uk>
2022-05-15 20:11:35 +0100

    Comment in index.jsx removed, was causing a build failure

--- settings/index.jsx
+++ settings/index.jsx
@@ -1,12 +1,6 @@
 function Settings(props) {
   return (
     <Page>
-      <!-- TODO: Add support for minutes -->
-<!--         <TextInput
-          label="Duration (minutes)"
-          settingsKey="minutes"
-        /> -->
-      
         <TextInput
           label="Duration (seconds)"
           settingsKey="seconds"
